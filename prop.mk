d60dcc7056584ea89b19c48a7a16afd1285eea40
scarymen1325 <kyslenko3@outlook.com>
2020-05-20 05:02:15 +0300

    daisy: some fixes

--- prop.mk
+++ prop.mk
@@ -84,6 +84,7 @@ persist.vendor.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=2 \
 vidc.enc.dcvs.extra-buff-count=2 \
 vendor.camera.lowpower.record.enable=1 \
+persist.vendor.camera.HAL3.enabled=1 \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.huaqin.factory,org.lineageos.snap \
 vendor.camera.aux.packagelist2=com.android.systemui,com.huaqin.cameraautotest,com.huaqin.runtime \
 vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp,com.android.camera2 \
