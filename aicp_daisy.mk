9aefe1047ef0f6d4a48f0f36b06b0ad3287739f9
David <davzeller@gmail.com>
2020-05-27 17:04:36 +0200

    Adding aicp_daisy.mk

---
+++ aicp_daisy.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Inherit from daisy device
+$(call inherit-product, device/xiaomi/daisy/device.mk)
+
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := daisy
+PRODUCT_NAME := aicp_daisy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A2 Lite
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_VENDOR := xiaomi
+TARGET_VENDOR_PRODUCT_NAME := daisy
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="daisy" \
+    TARGET_DEVICE="daisy"
+
+
